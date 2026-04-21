5d9052326db2e697c392d2d39949a57573272a78
Lucas007525 <lucas.ferraz.barros@escola.pr.gov.br>
2026-04-21 14:04:01 -0300

    Add files via upload

---
+++ estruturaderepetição.py
@@ -0,0 +1,9 @@
+numero = int(input ("Digite um número:"))
+#Aqui eu pedi um número pro meu usuário.
+lista = list(range(0, numero + 1))
+#aqui eu criei uma lista de 0 até o número digitado.
+for valor in lista:
+    print(valor)
+#Aqui o for percorreu toda a lista e o print mostrou cada valor dela.
+
+#O código todo cria uma lista de 0 até o número que meu usuário digitou e mostra todos eles.
